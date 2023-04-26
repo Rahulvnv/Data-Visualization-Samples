c5b12b6c6960a23c28d640b73ad43710a37bb5a2
Rahul VNV <98905098+Rahulvnv@users.noreply.github.com>
2023-04-26 19:22:07 +0530

    Update Juventus_Dashboard.py

--- Juventus_Dashboard.py
+++ Juventus_Dashboard.py
@@ -219,10 +219,10 @@ if(selected_viz_type=="Player Report"):
                     a = matchDD[i]['value']
             return (a)
         team_plot_data = eventsdf[eventsdf["matchId"] == matchId_giver(selected_match)]
-        if(df[df['matchId']==matchId_giver(selected_match)]['home_team']=='Juventus']:
-           opponent=df[df['matchId']==matchId_giver(selected_match)]['away_team']
+        if(df[df['matchId']==matchId_giver(selected_match)]['home_team']=='Juventus']):
+            opponent=df[df['matchId']==matchId_giver(selected_match)]['away_team']
         else:
-           opponent=df[df['matchId']==matchId_giver(selected_match)]['home_team']
+            opponent=df[df['matchId']==matchId_giver(selected_match)]['home_team']
         players = team_plot_data["playerName"].unique().tolist()
         players = [x for x in players if str(x) != 'nan']
         st.sidebar.header('Player Input Tab')
