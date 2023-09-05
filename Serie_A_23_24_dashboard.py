c04a465d8986d669ea434a0dddc909404009a8e7
Rahul VNV <98905098+Rahulvnv@users.noreply.github.com>
2023-09-05 15:06:25 +0530

    Update Serie_A_23_24_dashboard.py

--- Serie_A_23_24_dashboard.py
+++ Serie_A_23_24_dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import mplsoccer as mps
 import pandas as pd
-import plotly.express as px
 from PIL import Image
 from urllib.request import urlopen
 import plotly.graph_objects as go
